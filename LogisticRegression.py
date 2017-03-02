321f193c1c0305eafae083c8b03f6b6c6c984872
Kah Siong Tan <jax79sg@yahoo.com.sg>
2017-03-02 00:23:58 +0000

    LogisticRegression - Model Train

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -1,31 +1,33 @@
 ## Linear model skeleton for Problem 3.
 ## version 0.1
 
-
-
 """
 ## Overview for problem 3 (Not sure if i understood it correctly)
-1. Create a logistic regression model for CTR estimation
+1. Create a logistic regression model for CTR estimation (Cos predicting for click=1 or 0, so use logRegression)
     y ~ x
     where y = click
     where x = a list of variables we choose
-    - disregard accuracy for now (Means will not use validation data... Will fix/update in next version)
-
+    TODO:
+        Fixed - disregard accuracy for now (Means will not use validation data... Will fix/update in next version)
+        - Still cannot handle alphabetical variables e.g. useragent .... Will fix/update in next version.
+        - The data has too many click=0, trained model is skewed. To refine train model
 2. CTR estimation
-- Estimate click for every record in the test set using the model above
-    - Assume all same advertiser for now (Will fix in next version)
-- Compute pCTR = sumofclicks/alltestrecords
+    - Estimate click for every record in the test set using the model above
+    - Compute pCTR = sumofclicks/alltestrecords
+    TODO:
+        - Assume all same advertiser for now (Will fix in next version)
 
 3. Compute bid = base_bid x pCTR/avgCTR
-- A bit lost here, pCTR and avgCTR ??
+    - A bit lost here, what's avgCTR ??
 
 """
-
 
 import numpy as np
 from patsy import patsy
 from sklearn.linear_model import LogisticRegression
 import ipinyouReader as ipinyouReader
+from ipinyouWriter import ResultWriter as ResultWriter
+from sklearn import metrics
 
 # #List of column names. To be copy and pasted (as needed) in the formula for logistic regression
 # click='click'
@@ -56,29 +58,66 @@ import ipinyouReader as ipinyouReader
 # usertag='usertag'
 
 
+##########################
+## Modelling and training
 
 # load dataset
 print("Reading dataset...")
-trainDF = ipinyouReader.ipinyouReader("../dataset/train.csv").getDataFrame()
+trainDF = ipinyouReader.ipinyouReader("../dataset/trainPruned.csv").getDataFrame()
 
 print("Setting up Y and X for logistic regression")
-y, X =patsy.dmatrices('click ~ weekday + hour + region + city + adexchange + useragent',trainDF, return_type="dataframe")
-print (X.columns)
-
+yTrain, xTrain =patsy.dmatrices('click ~ weekday + hour + region + city + adexchange +slotwidth + slotheight + slotprice + advertiser',trainDF, return_type="dataframe")
+print((xTrain.columns))
+print ("No of features in input matrix: %d" % len(xTrain.columns))
 
 # flatten y into a 1-D array
 print("Flatten y into 1-D array")
-y = np.ravel(y)
-
-
+yTrain = np.ravel(yTrain)
 
 # instantiate a logistic regression model, and fit with X and y
 model = LogisticRegression()
 print("Training Model...")
-model = model.fit(X, y)
+model = model.fit(xTrain, yTrain)   #Loss function:liblinear
 
 # check the accuracy on the training set
-print("Training acccuracy: %5.3f" % model.score(X, y))
+print("\n\nTraining acccuracy: %5.3f" % model.score(xTrain, yTrain))
+
+
+########################
+## Prediction of validation set (Clicks)
+print("Reading validation set")
+validateDF = ipinyouReader.ipinyouReader("../dataset/validationPruned.csv").getDataFrame()
+
+
+print("Setting up X Y validation for prediction")
+yValidate, xValidate =patsy.dmatrices('click ~ weekday + hour + region + city + adexchange +slotwidth + slotheight + slotprice + advertiser',validateDF, return_type="dataframe")
+print ("No of features in input matrix: %d" % len(xValidate.columns))
+
+# predict click labels for the validation set
+print("Predicting...")
+predicted = model.predict(xValidate) #0.5 prob threshold
+print("Writing to csv")
+valPredictionWriter=ResultWriter()
+valPredictionWriter.writeResult(filename="predictValidate.csv", data=predicted)
+print ("\n\nPrediction acc on validation set: %f5.3" % metrics.accuracy_score(yValidate, predicted))
+
+########################
+## Prediction of test set (Clicks)
+print("Reading test set")
+testDF = ipinyouReader.ipinyouReader("../dataset/test.csv").getDataFrame()
+
+print("Setting up X test for prediction")
+xTest =patsy.dmatrix('weekday + hour + region + city + adexchange +slotwidth + slotheight + slotprice + advertiser',testDF, return_type="dataframe")
+print ("No of features in input matrix: %d" % len(xValidate.columns))
+
+# predict click labels for the test set
+print("Predicting...")
+predicted = model.predict(xTest) #0.5 prob threshold
+print(type(predicted))
+print("Writing to csv")
+testPredictionWriter=ResultWriter()
+testPredictionWriter.writeResult(filename="predictTest.csv", data=predicted)
+
 
 
 
