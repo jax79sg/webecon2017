0f32c4aa150cb1d751e406d76e2e9d86bad4fb23
jax79sg <jax79sg@yahoo.com.sg>
2017-03-05 21:33:33 +0000

    Merge branch 'master' of https://github.com/jax79sg/webecon2017
    # Conflicts:
    #	LogisticRegressionBidModel.py

---
+++ UserException.py
@@ -0,0 +1,12 @@
+
+class ModelNotTrainedException(Exception):
+    """Exception raised for untrained models.
+
+    Attributes:
+        expression -- input expression in which the error occurred
+        message -- explanation of the error
+    """
+
+    def __init__(self, expression, message):
+        self.expression = expression
+        self.message = message
