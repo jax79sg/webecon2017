6d552177bcd2b23ad1f7190055f7b08eb90fcb9f
cspoh <pchunsio@gmail.com>
2017-03-12 21:55:31 +0000

    Added AUC and RMSE

--- Evaluator.py
+++ Evaluator.py
@@ -3,71 +3,10 @@ from collections import defaultdict
 import time
 import numpy as np
 from sklearn import metrics
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc, mean_squared_error
 
 class Evaluator():
-    # def __init__(self, budget, ourBids, goldlabels):
-    #     self.budget = budget
-    #     self.ourBids = ourBids
-    #     self.goldlabels = goldlabels
-
-    # def computePerformanceMetrics(self):
-    #     print("************************************************")
-    #     print("Try not to use this. Use computePerformanceMetricsDF(self, budget, ourBidsDF, goldlabelsDF) instead")
-    #     print("************************************************")
-    #     self.resultDict = defaultdict(float)
-    #
-    #     # start_time = time.time()
-    #     # resultIndex = 0
-    #     # for i in range(self.goldlabels.shape[0]):
-    #     #     # Check if bid id match?
-    #     #     if self.ourBids[resultIndex][0] == self.goldlabels[i][3]:
-    #     #
-    #     #         # Check if bid price is greater than (or eq) gold's payprice - TA said it's greater or eq to payprice = win
-    #     #         if int(self.ourBids[resultIndex][1]) >= int(self.goldlabels[i][22]):
-    #     #             # Won the bid!!
-    #     #             self.resultDict['won'] += 1
-    #     #             # Add the pay price for this ad. Don't Div by 1000 to convert to Chinese fen #budget is x 1000 already
-    #     #             self.resultDict['spend'] += (self.goldlabels[i][22])
-    #     #
-    #     #             # Check if gold is clicked
-    #     #             if int(self.goldlabels[i][0]) == 1:
-    #     #                 # Increment click counter
-    #     #                 self.resultDict['click'] += 1
-    #     #
-    #     #         resultIndex += 1
-    #     #
-    #     #     # Stop when reach end of our bids or budget are used
-    #     #     if resultIndex == self.ourBids.shape[0] or self.resultDict['spend'] >= self.budget:
-    #     #         break
-    #     #
-    #     #
-    #     # #print("Result Matched: ", resultIndex, " out of ", self.ourBids.shape[0])
-    #     # print('Metrics compute time: {} seconds'.format(round(time.time() - start_time, 2)))
-    #
-    #     start_time = time.time()
-    #     bidids_match=np.all(np.equal(self.ourBids[:, 0], self.goldlabels[:, 3])) #safety check bidids match
-    #     if bidids_match:
-    #         #if bid price is greater than (or eq) gold's payprice - TA said it's greater or eq to payprice = win
-    #         wonbid=np.greater_equal(self.ourBids[:,1].astype(int),self.goldlabels[:,22])
-    #         spend=self.goldlabels[wonbid,22] #use wonbid as mask
-    #         clicks=self.goldlabels[wonbid,0] #use wonbid as mask
-    #
-    #         #work backwards until we are within budget
-    #         i=-1
-    #         while sum(spend) > self.budget:
-    #             #print("{},{},{}".format(i,sum(spend),self.budget))
-    #             spend[i] = 0
-    #             clicks[i] = 0
-    #             wonbid[i] = False
-    #             i+=-1
-    #
-    #         self.resultDict={'won':sum(wonbid),'click':sum(clicks),'spend':sum(spend)} #TODO: verify spend consistency
-    #     else:
-    #         print("Bid Ids did not match in arrays")
-    #
-    #     print('Metrics compute time: {} seconds'.format(round(time.time() - start_time, 2)))
-    #     return self.resultDict
-
     def computePerformanceMetricsDF(self, budget, ourBidsDF, goldlabelsDF,verbose=False):
 
         self.resultDict = defaultdict(float)
@@ -161,3 +100,27 @@ class ClickEvaluator():
         # print("Focus on click=1 recall score and Number of 1 in pred.")
 
         return p[i], r[i], f1[i]#, (clicked/np.count_nonzero(y_Pred))
+
+    # Plot data https://vkolachalama.blogspot.co.uk/2016/05/keras-implementation-of-mlp-neural.html
+    def roc_results_plot(self, y_true, y_pred_prob, showGraph=True):
+        fpr, tpr, _ = roc_curve(y_true, y_pred_prob)
+        roc_auc = auc(fpr, tpr)
+        if showGraph:
+            plt.figure()
+            plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
+            plt.plot([0, 1], [0, 1], 'k--')
+            plt.xlim([0.0, 1.05])
+            plt.ylim([0.0, 1.05])
+            plt.xlabel('False Positive Rate')
+            plt.ylabel('True Positive Rate')
+            plt.title('Receiver operating characteristic curve')
+
+            plt.show()
+        print('AUC: %f' % roc_auc)
+
+    def printRMSE(self, y_Pred, y_Gold):
+        mse = mean_squared_error(y_Gold, y_Pred)
+
+        rmse = mse ** 0.5
+
+        print("RMSE: %.5f" %rmse)
