68fe881aa61d7c9b6648a7985e66cde47d7e63c9
jax79sg <jax79sg@yahoo.com.sg>
2017-03-17 23:52:34 +0000

    Added comments

--- Utilities.py
+++ Utilities.py
@@ -5,10 +5,20 @@ class Utility():
 
     starttime=None
     def startTimeTrack(self):
+        """
+        This must be the first method to call before calling stopTimeTrack or checkpointTimeTrack
+        Will start the recording of time.
+        :return:
+        """
         self.starttime=datetime.datetime.now()
         pass
 
     def stopTimeTrack(self):
+        """
+        This is called in pair with startTimeTrack everytime.
+        It will print time lapse after startTimeTrack
+        :return:
+        """
         endtime=datetime.datetime.now()
         duration=endtime-self.starttime
         result="Time taken: ",duration.seconds," secs"
@@ -18,6 +28,16 @@ class Utility():
 
 
     def checkpointTimeTrack(self):
+        """
+        This can be called consecutively for as many times as long as startTimeTrack has been first called.
+        It will print the time lapse from last check point
+        E.g.
+        Utility().startTimeTrack)_
+        Utility().checkpointTimeTrack()
+        Utility().checkpointTimeTrack()
+        Utility().checkpointTimeTrack()
+        :return:
+        """
         endtime = datetime.datetime.now()
         duration = endtime - self.starttime
         result = "Time taken: ", duration.seconds, " secs"
